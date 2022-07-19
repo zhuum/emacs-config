7331f068ebb388dfe03392aaa1a9e0a024ba0d0c
Marcus McKinnon <marcus.mckinnon@halliburton.com>
2022-07-19 16:24:50 -0500

    Updated for crafted-emacs changes

--- config.el
+++ config.el
@@ -1,115 +1,80 @@
-(require 'rational-defaults)
-(require 'rational-ui)
-(require 'rational-editing)
-(require 'rational-evil)
-(require 'rational-completion)
-(require 'rational-windows)
-
-;;; code:
-(straight-use-package 'use-package)
-(require 'package)
-
-;; Emacs 27.x has the gnu elpa as the default for `package-archives'
-;; Emacs 28.x has the gnu and nongnu elpa as the default.
-;; Only add nongnu elpa for Emacs version < 28
-(when (version< emacs-version "28.0")
-  (add-to-list 'package-archives (cons "nongnu" "https://elpa.nongnu.org/nongnu/")))
-
-;; Always add melpa to `package-archives'
-(add-to-list 'package-archives (cons "melpa" "https://melpa.org/packages/"))
-
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-
-
-
-;; Set further font and theme customizations
-(set-face-attribute 'default nil
-                  :font "JetBrains Mono"
-                  :weight 'light
-                  :height 185)
-
-;; fixes the blue default colors when starting
-(setf default-frame-alist (assq-delete-all 'background-color default-frame-alist))
-(setf default-frame-alist (assq-delete-all 'foreground-color default-frame-alist))
-(load-theme 'doom-nord t)
-
-(set-face-attribute 'region nil :background "#5e81ac" :foreground "#dae2eb")
-(set-face-attribute 'lazy-highlight nil :background "#5e81ac" :foreground "#dae2eb")
+;; general
+(require 'crafted-defaults)    ; Sensible default settings for Emacs
+(require 'crafted-updates)     ; Tools to upgrade Crafted Emacs
+(require 'crafted-use-package) ; Configuration for `use-package`
+(require 'crafted-compile)     ; automatically compile some emacs lisp files
+(require 'crafted-speedbar)
 
 (setq-default tab-width 4)
 (setq-default indent-tabs-mode nil)
-;; not sure how to remap tab to indent, "M-i" & "M-o" are the defaults
-;; (define-key evil-insert-state-map (kbd "<tab>") 'evil-jump-forward)
-;; (define-key evil-insert-state-map (kbd "S-<tab>") 'evil-jump-backward)
+(setq-default scroll-margin 8)
+
+;; UI
+(require 'crafted-ui)          ; Better UI experience (modeline etc.)
+(require 'crafted-editing)     ; Whitspace trimming, auto parens etc.
+(require 'crafted-windows)     ; Window management configuration
+(require 'crafted-completion)  ; selection framework based on `vertico`
+(require 'crafted-evil)        ; An `evil-mode` configuration
+
+(crafted-package-install-package 'doom-themes)
+(disable-theme 'deeper-blue)
+(load-theme 'doom-one t)
+
+;; org mode
+(require 'crafted-org)         ; org-appear, clickable hyperlinks etc.
+
+;; project
+(require 'crafted-project)     ; built-in alternative to projectile
+
+;; IDE
+(require 'crafted-ide)
+(crafted-package-install-package 'typescript-mode)
+
+;; Shellse
+(require 'crafted-shells)
+
+;; not sure if needed
+;; (add-hook 'python-mode-hook 'eglot-ensure)
+;; (add-hook 'typescript-mode-hook 'eglot-ensure)
 
 ;; keybinds
-(defcustom rational-windows-evil-style nil
-  "When t, window movement bindings will be evil-style.")
 
-(defcustom rational-windows-prefix-key "C-c w"
-  "Configure the prefix key for `rational-windows' bindings.")
+(custom-set-variables '(evil-want-C-u-scroll t)
+                      '(crafted-evil-discourage-arrow-keys t))
 
-(winner-mode 1)
+(define-key vertico-map (kbd "C-f") 'vertico-exit)
 
-(define-prefix-command 'rational-windows-key-map)
+(define-key corfu-map (kbd "C-j") 'corfu-next)
+(define-key corfu-map (kbd "C-k") 'corfu-previous)
 
-(define-key 'rational-windows-key-map (kbd "k") 'windmove-down)
-(define-key 'rational-windows-key-map (kbd "j") 'windmove-up)
-(define-key 'rational-windows-key-map (kbd "h") 'windmove-left)
-(define-key 'rational-windows-key-map (kbd "l") 'windmove-right)
+(setq mac-option-key-is-meta nil
+      mac-command-key-is-meta t
+      mac-command-modifier 'meta
+      mac-option-modifier 'none)
 
+(global-set-key (kbd "C-M-j") 'consult-buffer)
+(global-set-key (kbd "C-M-r") 'consult-ripgrep)
+(global-set-key (kbd "M-/") 'evilnc-comment-or-uncomment-lines)
 
 ;; modules
-
-(straight-use-package 'restclient)
-(straight-use-package 'ob-restclient)
+;; rest client
+(crafted-package-install-package 'restclient)
+(crafted-package-install-package 'ob-restclient)
 
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((restclient . t)))
 
-(use-package which-key
-  :ensure t
-  :config
-  (which-key-mode))
 
-;;; LSP
-;; (use-package lsp-mode
-;;   :init
-;;   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-;;   (setq lsp-keymap-prefix "C-c l")
-;;   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-;;          (python-mode . lsp)
-;;          ;; if you want which-key integration
-;;          (lsp-mode . lsp-enable-which-key-integration))
-;;   :commands lsp)
+;;(use-package coffee-mode
+;;  :ensure t)
 
-;; (use-package lsp-python-ms
-;;   :ensure t
-;;   :init (setq lsp-python-ms-auto-install-server t)
-;;   :hook (python-mode . (lambda ()
-;;                           (require 'lsp-python-ms)
-;;                           (lsp))))  ; or lsp-deferred
+;;(use-package sws-mode
+;;  :ensure t)
 
-;; LSP
-(use-package eglot
-  :ensure t)
-
-(add-hook 'python-mode-hook 'eglot-ensure)
-(add-hook 'html-mode-hook 'eglot-ensure)
-(add-hook 'typescript-mode-hook 'eglot-ensure)
-(add-hook 'javascript-mode-hook 'eglot-ensure)
-
-(use-package coffee-mode
-  :ensure t)
-
-(use-package sws-mode
-  :ensure t)
-(use-package stylus-mode
-  :ensure t
-  :requires sws-mode)
+;;(use-package stylus-mode
+;;  :ensure t
+;;  :requires sws-mode)
 
 (use-package magit
   :ensure t
@@ -118,212 +83,6 @@
   :bind (("C-x g" . magit-status)
          ("C-x C-g" . magit-status)))
 
-(setq magit-display-buffer-function
-      (lambda (buffer)
-        (display-buffer
-         buffer (if (and (derived-mode-p 'magit-mode)
-                         (memq (with-current-buffer buffer major-mode)
-                               '(magit-process-mode
-                                 magit-revision-mode
-                                 magit-stash-mode
-                                 magit-status-mode)))
-                    nil
-                  '(display-buffer-same-window)))))
-
-
-(use-package corfu
-  :ensure t
-  ;; Optional customizations
-  :custom
-  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
-  (corfu-auto t)                 ;; Enable auto completion
-  ;; (corfu-separator ?\s)          ;; Orderless field separator
-  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
-  ;; (corfu-preselect-first nil)    ;; Disable candidate preselection
-  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
-  ;; (corfu-echo-documentation nil) ;; Disable documentation in the echo area
-  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
-
-  ;; You may want to enable Corfu only for certain modes.
-  ;; :hook ((prog-mode . corfu-mode)
-  ;;        (shell-mode . corfu-mode)
-  ;;        (eshell-mode . corfu-mode))
-
-  ;; Recommended: Enable Corfu globally.
-  ;; This is recommended since dabbrev can be used globally (M-/).
-  :init
-  (corfu-global-mode))
-
-(use-package cape
-  :ensure t
-  ;; Bind dedicated completion commands
-  :bind (("C-c p p" . completion-at-point) ;; capf
-         ("C-c p t" . complete-tag)        ;; etags
-         ("C-c p d" . cape-dabbrev)        ;; or dabbrev-completion
-         ("C-c p f" . cape-file)
-         ("C-c p k" . cape-keyword)
-         ("C-c p s" . cape-symbol)
-         ("C-c p a" . cape-abbrev)
-         ("C-c p i" . cape-ispell)
-         ("C-c p l" . cape-line)
-         ("C-c p w" . cape-dict)
-         ("C-c p \\" . cape-tex)
-         ("C-c p _" . cape-tex)
-         ("C-c p ^" . cape-tex)
-         ("C-c p &" . cape-sgml)
-         ("C-c p r" . cape-rfc1345))
-  :init
-  ;; Add `completion-at-point-functions', used by `completion-at-point'.
-  (add-to-list 'completion-at-point-functions #'cape-file)
-  (add-to-list 'completion-at-point-functions #'cape-tex)
-  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
-  (add-to-list 'completion-at-point-functions #'cape-keyword)
-  ;;(add-to-list 'completion-at-point-functions #'cape-sgml)
-  ;;(add-to-list 'completion-at-point-functions #'cape-rfc1345)
-  ;;(add-to-list 'completion-at-point-functions #'cape-abbrev)
-  ;;(add-to-list 'completion-at-point-functions #'cape-ispell)
-  ;;(add-to-list 'completion-at-point-functions #'cape-dict)
-  ;;(add-to-list 'completion-at-point-functions #'cape-symbol)
-  ;;(add-to-list 'completion-at-point-functions #'cape-line)
-)
-
-;;; org mode
-(straight-use-package '(org-modern :host github :repo "minad/org-modern"))
-(add-hook 'org-mode-hook #'org-modern-mode)
-(setq org-startup-folded nil)
-
-;;; snippets
-(use-package yasnippet
-  :ensure t
-  :config
-  (yas-global-mode 1))
-
-(use-package yasnippet-snippets
-  :ensure t)
-
-;; Example configuration for Consult
-(use-package consult
-  :ensure t
-  ;; Replace bindings. Lazily loaded due by `use-package'.
-  :bind (;; C-c bindings (mode-specific-map)
-         ("C-c h" . consult-history)
-         ("C-c m" . consult-mode-command)
-         ("C-c k" . consult-kmacro)
-         ;; C-x bindings (ctl-x-map)
-         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
-         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
-         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
-         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
-         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
-         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
-         ;; Custom M-# bindings for fast register access
-         ("M-#" . consult-register-load)
-         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
-         ("C-M-#" . consult-register)
-         ;; Other custom bindings
-         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
-         ("<help> a" . consult-apropos)            ;; orig. apropos-command
-         ;; M-g bindings (goto-map)
-         ("M-g e" . consult-compile-error)
-         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
-         ("M-g g" . consult-goto-line)             ;; orig. goto-line
-         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
-         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
-         ("M-g m" . consult-mark)
-         ("M-g k" . consult-global-mark)
-         ("M-g i" . consult-imenu)
-         ("M-g I" . consult-imenu-multi)
-         ;; M-s bindings (search-map)
-         ("M-s d" . consult-find)
-         ("M-s D" . consult-locate)
-         ("M-s g" . consult-grep)
-         ("M-s G" . consult-git-grep)
-         ("M-s r" . consult-ripgrep)
-         ("M-s l" . consult-line)
-         ("M-s L" . consult-line-multi)
-         ("M-s m" . consult-multi-occur)
-         ("M-s k" . consult-keep-lines)
-         ("M-s u" . consult-focus-lines)
-         ;; Isearch integration
-         ("M-s e" . consult-isearch-history)
-         :map isearch-mode-map
-         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
-         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
-         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
-         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
-         ;; Minibuffer history
-         :map minibuffer-local-map
-         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
-         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
-
-  ;; Enable automatic preview at point in the *Completions* buffer. This is
-  ;; relevant when you use the default completion UI.
-  :hook (completion-list-mode . consult-preview-at-point-mode)
-
-  ;; The :init configuration is always executed (Not lazy)
-  :init
-
-  ;; Optionally configure the register formatting. This improves the register
-  ;; preview for `consult-register', `consult-register-load',
-  ;; `consult-register-store' and the Emacs built-ins.
-  (setq register-preview-delay 0.5
-        register-preview-function #'consult-register-format)
-
-  ;; Optionally tweak the register preview window.
-  ;; This adds thin lines, sorting and hides the mode line of the window.
-  (advice-add #'register-preview :override #'consult-register-window)
-
-  ;; Optionally replace `completing-read-multiple' with an enhanced version.
-  (advice-add #'completing-read-multiple :override #'consult-completing-read-multiple)
-
-  ;; Use Consult to select xref locations with preview
-  (setq xref-show-xrefs-function #'consult-xref
-        xref-show-definitions-function #'consult-xref)
-
-  ;; Configure other variables and modes in the :config section,
-  ;; after lazily loading the package.
-  :config
-
-  ;; Optionally configure preview. The default value
-  ;; is 'any, such that any key triggers the preview.
-  ;; (setq consult-preview-key 'any)
-  ;; (setq consult-preview-key (kbd "M-."))
-  ;; (setq consult-preview-key (list (kbd "<S-down>") (kbd "<S-up>")))
-  ;; For some commands and buffer sources it is useful to configure the
-  ;; :preview-key on a per-command basis using the `consult-customize' macro.
-  (consult-customize
-   consult-theme
-   :preview-key '(:debounce 0.2 any)
-   consult-ripgrep consult-git-grep consult-grep
-   consult-bookmark consult-recent-file consult-xref
-   consult--source-bookmark consult--source-recent-file
-   consult--source-project-recent-file
-   :preview-key (kbd "M-."))
-
-  ;; Optionally configure the narrowing key.
-  ;; Both < and C-+ work reasonably well.
-  (setq consult-narrow-key "<") ;; (kbd "C-+")
-
-  ;; Optionally make narrowing help available in the minibuffer.
-  ;; You may want to use `embark-prefix-help-command' or which-key instead.
-  ;; (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
-
-  ;; By default `consult-project-function' uses `project-root' from project.el.
-  ;; Optionally configure a different project root function.
-  ;; There are multiple reasonable alternatives to chose from.
-  ;;;; 1. project.el (the default)
-  ;; (setq consult-project-function #'consult--default-project--function)
-  ;;;; 2. projectile.el (projectile-project-root)
-  ;; (autoload 'projectile-project-root "projectile")
-  ;; (setq consult-project-function (lambda (_) (projectile-project-root)))
-  ;;;; 3. vc.el (vc-root-dir)
-  ;; (setq consult-project-function (lambda (_) (vc-root-dir)))
-  ;;;; 4. locate-dominating-file
-  ;; (setq consult-project-function (lambda (_) (locate-dominating-file "." ".git")))
-)
-
-(setq scroll-margin 8)
-(setq evil-move-cursor-back nil)
-
-(setq electric-pair-mode nil)
+(crafted-package-install-package 'minions)
+(use-package minions
+  :config (minions-mode 1))
