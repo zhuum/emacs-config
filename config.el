d7eb59201a181a7c44e302b27465cc77cd7c86b2
Marcus McKinnon <marcus.mckinnon@halliburton.com>
2022-07-28 12:08:13 -0500

    General updates

--- config.el
+++ config.el
@@ -1,4 +1,4 @@
-;; general
+;;; general
 (require 'crafted-defaults)    ; Sensible default settings for Emacs
 (require 'crafted-updates)     ; Tools to upgrade Crafted Emacs
 (require 'crafted-use-package) ; Configuration for `use-package`
@@ -9,7 +9,11 @@
 (setq-default indent-tabs-mode nil)
 (setq-default scroll-margin 8)
 
-;; UI
+(global-undo-tree-mode)
+(setq undo-tree-auto-save-history t)
+(setq undo-tree-history-directory-alist '(("." . "~/.emacs.undo.d/")))
+
+;;; UI
 (require 'crafted-ui)          ; Better UI experience (modeline etc.)
 (require 'crafted-editing)     ; Whitspace trimming, auto parens etc.
 (require 'crafted-windows)     ; Window management configuration
@@ -20,22 +24,41 @@
 (disable-theme 'deeper-blue)
 (load-theme 'doom-one t)
 
-;; org mode
+;;; org mode
 (require 'crafted-org)         ; org-appear, clickable hyperlinks etc.
 
-;; project
+;;; project
 (require 'crafted-project)     ; built-in alternative to projectile
 
-;; IDE
+;;; IDE
 (require 'crafted-ide)
 (crafted-package-install-package 'typescript-mode)
+(crafted-package-install-package 'python-black)
+(use-package python-black
+  :demand t
+  :after python
+  :hook (python-mode . python-black-on-save-mode-enable-dwim))
 
-;; Shellse
-(require 'crafted-shells)
+(crafted-package-install-package 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+
+;;; Shells
+(crafted-package-install-package 'vterm)
+;; configure vterm for evil-mode
+(use-package vterm
+  :demand t)
+(with-eval-after-load 'evil
+  (advice-add 'evil-collection-vterm-insert :before #'vterm-reset-cursor-point))
 
 ;; not sure if needed
 ;; (add-hook 'python-mode-hook 'eglot-ensure)
 ;; (add-hook 'typescript-mode-hook 'eglot-ensure)
+
+
+;;; notes
+(crafted-package-install-package 'denote)
+(require 'denote)
+(setq denote-directory "~/org")
 
 ;; keybinds
 
@@ -44,8 +67,11 @@
 
 (define-key vertico-map (kbd "C-f") 'vertico-exit)
 
+(define-key evil-motion-state-map (kbd "C-f") nil)
+
 (define-key corfu-map (kbd "C-j") 'corfu-next)
 (define-key corfu-map (kbd "C-k") 'corfu-previous)
+(define-key corfu-map (kbd "C-f") 'corfu-quit)
 
 (setq mac-option-key-is-meta nil
       mac-command-key-is-meta t
@@ -53,6 +79,7 @@
       mac-option-modifier 'none)
 
 (global-set-key (kbd "C-M-j") 'consult-buffer)
+(global-set-key (kbd "C-M-k") 'kill-current-buffer)
 (global-set-key (kbd "C-M-r") 'consult-ripgrep)
 (global-set-key (kbd "M-/") 'evilnc-comment-or-uncomment-lines)
 
